4c93e8855aab6443f2034af6af001f9347cdc153
juligarji <jgarzonj@unal.edu.co>
2018-04-11 23:50:26 -0500

    agregando gema carrierwave y agregando al controlador

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412042313) do
+ActiveRecord::Schema.define(version: 20180412044344) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
